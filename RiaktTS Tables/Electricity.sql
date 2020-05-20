c4e38aa2a00eeb722056b321d00322d5e985b247
Tomas Ukrinas <t.ukrinas@ba.lt>
2020-05-20 13:16:01 +0300

    Riak Tables update

--- RiaktTS Tables/Electricity.sql
+++ RiaktTS Tables/Electricity.sql
@@ -1,11 +1,12 @@
-CREATE TABLE Electricity
+CREATE TABLE ElectricityProduction
 (
+  Country      VARCHAR   NOT NULL,
   SerialNo     VARCHAR   NOT NULL,
   DeviceName   VARCHAR,
   Time         TIMESTAMP NOT NULL,
   Value        DOUBLE    NOT NULL,
   PRIMARY KEY (
-    (SerialNo, QUANTUM(Time, 1, 'd')),
-    SerialNo, Time DESC
+    (Country, QUANTUM(Time, 1, 'd')),
+    Country, Time DESC
   )
 );
